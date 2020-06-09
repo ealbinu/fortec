563af725270a85b445bb7da6ca5f8fa1e2cddbed
Albin Rodriguez <ealbinu@gmail.com>
2020-06-09 12:10:36 -0500

    update

--- app/app.js
+++ app/app.js
@@ -29,41 +29,12 @@ var app = new Vue({
         }
     },
     created () {
-        /*
-        for (var pr in productoslista) {
-            var prod = productoslista[pr]
-            this.categoriasex.push(prod.categoria1)
-            this.categoriasex.push(prod.categoria2)
+        console.log('created')
+        const urlParams = new URLSearchParams(window.location.search)
+        const correoenviado = urlParams.get('enviado')
+        console.log(correoenviado)
+        if (correoenviado!=null) {
+            this.$buefy.dialog.alert({message:'Tu mensaje ha sido enviado.', type: 'is-info'})
         }
-        */
-        /*
-        var contador = 0
-        for (var pr in productoslista) {
-            var prod = productoslista[pr]
-            //Categorias
-            var cats = prod.categorias.split('>')
-            if (cats.length) {
-                prod.categoria1 = cats[0]
-                prod.categoria2 = cats[1]
-            } else {
-                prod.categoria1 = prod.categorias
-            }
-            //Imagenes
-            var img = prod.image.split('|')
-            if (img.length) {
-                prod.image1 = img[0]
-                //prod.image2 = img[1]
-            } else {
-                prod.image1 = prod.image
-            }
-            this.$delete(prod, 'categorias')
-            this.$delete(prod, 'image')
-            this.$delete(prod, 'id')
-            this.$delete(prod, 'sku')
-            contador += 1
-            this.productos.push(prod)
-        }
-        console.log('Productos: ', contador)
-        */
     }
 })
